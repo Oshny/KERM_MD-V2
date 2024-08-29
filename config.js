f3dfc4e55f40102d2bd28a5ebb9e2340ed60445b
Oshny <168451673+Oshny@users.noreply.github.com>
2024-08-29 18:38:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -13,13 +13,13 @@ global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Douala";
-global.github = process.env.GITHUB || "https://github.com/Kgtech-cmr/KERM_MD-V2";
+global.github = process.env.GITHUB || "https://github.com/Kgtech-cmr/KAKASHI BELZÉBUTH-V2";
 global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029Vafn6hc7DAX3fzsKtn45";
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vafn6hc7DAX3fzsKtn45";
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/40ffc2a288542560c887c.jpg";
-global.devs = "https://wa.me/237650564445 , https://wa.me/237656520674";
-global.sudo = process.env.SUDO || "237650564445";
-global.owner = process.env.OWNER_NUMBER || "237656520674";
+global.devs = "https://wa.me/33751504987 , https://wa.me/33751504987";
+global.sudo = process.env.SUDO || "33751504987";
+global.owner = process.env.OWNER_NUMBER || "33751504987";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -42,17 +42,17 @@ global.scan = "https://kerm-session-39mi.onrender.com/wasiqr";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  "PUT YOUR SESSION HERE"
+  "DV1hjb7D#ZKLIfftI36fSVo0jWFlUEPpPu-Vh-vMdl3kEIN_lczo"
 module.exports = {
   menu: process.env.MENU || "1",
-  HANDLERS: process.env.PREFIX || ".",
+  HANDLERS: process.env.PREFIX || "+",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "`BRAND PRODUCT OF ☞⌜ KG TECH⌝☜`",
-  author: process.env.PACK_AUTHER || "KG TECH",
-  packname: process.env.PACK_NAME || "Kerm-MD-V2",
-  botname: process.env.BOT_NAME || "*ᵏᵍ┘𝙺𝙴𝚁𝙼_𝙼𝙳_𝚅𝟸𖤐*",
-  ownername: process.env.OWNER_NAME || "☞⌜ KG TECH⌝☜",
+  caption: process.env.CAPTION || "`BRAND PRODUCT OF VILLAGE DE KONOHA",
+  author: process.env.PACK_AUTHER || "🌴༂࿔𝐾𝐴𝐾𝐴𝑆𝐻𝐼♚𝐵𝐸𝐿𝑍É𝐵𝑈𝑇𝐻𖤍 🌹",
+  packname: process.env.PACK_NAME || "KAKASHI BELZÉBUTH-MD-V2",
+  botname: process.env.BOT_NAME || "*🌴༂࿔𝐾𝐴𝐾𝐴𝑆𝐻𝐼♚𝐵𝐸𝐿𝑍É𝐵𝑈𝑇𝐻𖤍 🌹*",
+  ownername: process.env.OWNER_NAME || "🌴༂࿔𝐾𝐴𝐾𝐴𝑆𝐻𝐼♚𝐵𝐸𝐿𝑍É𝐵𝑈𝑇𝐻𖤍 🌹",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "tCxobE1FqJgfbDZgsYQNHBFB",
